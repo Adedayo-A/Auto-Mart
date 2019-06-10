b3450880f6327b4ed31cc4fdf1a86b844c6ab124
Adedayo-A <adesegunadedayo@hotmail.com>
2019-06-10 16:51:58 +0100

    chore(endpoint):create GET request endpoint for cars by body type [Finishes #166470132]

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,7 @@ const controllers = require('../controllers/car.js')
 const express = require('express');
 const router = express.Router();
 
-  router.get('/api/v1/car/', controllers.getCars);
-  router.get('/api/v1/car/:id', controllers.getCar);
-    
-  
-    // app.post('/api/v1/cars', controllers.addCars)
-    // app.put('/api/v1/cars/:id', controllers.modifySaleOrder)
-  
-  
+router.get('/api/v1/car/', controllers.getCars);
+router.get('/api/v1/car/:id', controllers.getCar);  
 
 module.exports = router;
