f825baa15e6594d50749c300c2f881eb19a56a49
Adedayo-A <adesegunadedayo@hotmail.com>
2019-06-10 15:22:01 +0100

    chore(endpoints):create endpoint for new and unsold cars [Finishes #166469631]

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,11 +6,9 @@ const router = express.Router();
   router.get('/api/v1/car/:id', controllers.getCar);
     
   
-  // app.post('/api/v1/cars', controllers.addCars)
+    // app.post('/api/v1/cars', controllers.addCars)
     // app.put('/api/v1/cars/:id', controllers.modifySaleOrder)
     // app.delete('/api/v1/cars/:id', controllers.deleteSaleOder)
-    // app.get('/api/v1/cars/:status', controllers.getAllUnsoldCars);
-    // app.get('/api/v1/cars/:priceRange', controllers.getCarsByPriceRange);
-    // router.get('/', book_controller.index)
+  
 
 module.exports = router;
