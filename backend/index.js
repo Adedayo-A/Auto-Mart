0d568ca3ffd9b324c06ef9a4d92e5347645bf217
Adedayo-A <adesegunadedayo@hotmail.com>
2019-06-10 10:46:39 +0100

    git commit -m "Implement Search API [Finishes #166470132 #166469827 #166469698 #166469631 #166469348 #166469192 #16646927]"

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const route = require('./routes/cars.js')
+const route = require('./routes/index.js')
 // const routeFunc = require('./routes/index.js');
 const app = express();
 app.use(cors());
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,20 @@
-const controllers = require('../controllers/index.js');
+const controllers = require('../controllers/car.js')
+const express = require('express');
 const router = express.Router();
 
-router.get('/api/v1/car/', controllers.getCars);
-router.get('/api/v1/car/:id', controllers.getACar);
+  router.get('/api/v1/car/', controllers.getCars);
+  router.get('/api/v1/car/:id', controllers.getCar);
     
-    // app.post('/api/v1/cars', controllers.addCars)
+  
+  // app.post('/api/v1/cars', controllers.addCars)
     // app.put('/api/v1/cars/:id', controllers.modifySaleOrder)
     // app.delete('/api/v1/cars/:id', controllers.deleteSaleOder)
     // app.get('/api/v1/cars/:status', controllers.getAllUnsoldCars);
     // app.get('/api/v1/cars/:priceRange', controllers.getCarsByPriceRange);
     // router.get('/', book_controller.index);
-    // const apiRoute = () => {
-    //  app.get('/api/v1/car/', controllers.getCars);
-    // }
+    
+// module.exports = (e) => {
+//      app.get('/api/v1/car/', controllers.getCars);
+// }
 
-module.exports = router
+module.exports = router;
