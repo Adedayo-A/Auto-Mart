fd3d4123789e4e79a357502d99d8e1f40897053e
Adedayo-A <adesegunadedayo@hotmail.com>
2019-06-25 00:04:26 +0100

    chore(database):Implement get all unsold cars endpoint with postgresql database [FINISHES #166883554]

--- backend/controllers/cars.js
+++ backend/controllers/cars.js
@@ -5,77 +5,37 @@ const { Client } = require('pg');
 const jwt = require('jsonwebtoken');
 const cars = require('../db/Cars.js');
 
-// const jwtKey = require('../bin/www');
-
-// class carControllers {
 // GET REQUESTS
 const getCars = (req, res) => {
-  let allCars = cars;
-  // PRICE-RANGE
-  if (req.query.min_price && req.query.max_price && req.query.status) {
-    const foundStatus = allCars.some(car => car.status === req.query.status);
-    if (foundStatus) {
-      allCars = allCars.filter(availableCar => availableCar.status === req.query.status);
+  // PRICE-RANGE AND STATUS-AVAILABLE
+  const pg = new Client({
+    connectionString: process.env.db_URL,
+  });
+  pg.connect();
+  // PG Connect
+  // eslint-disable-next-line consistent-return
+  const query = 'SELECT * FROM carads WHERE status = $1';
+  const value = ['available'];
+  pg.query(query, value, (err, dbres) => {
+    if (err) {
+      console.log(err.stack);
+      res.status(500).json({
+        message: 'error encountered',
+      });
+    } else if (dbres.rows.length === 0) {
+      res.status(404).json({
+        message: 'No car found!!!',
+      });
     } else {
-      res.status(404).json({ msg: `No car with the status of ${req.query.status}` });
+      const carad = dbres.rows;
+      res.status(200).json({
+        message: 'result completed',
+        carad,
+      });
     }
-    // eslint-disable-next-line max-len
-    const foundPrice = allCars.some(car => car.price >= parseInt(req.query.min_price, 10) && car.price <= parseInt(req.query.max_price, 10));
-    if (foundPrice) {
-      // eslint-disable-next-line max-len
-      allCars = allCars.filter(car => car.price >= parseInt(req.query.min_price, 10) && car.price <= parseInt(req.query.max_price, 10));
-      res.status(200).json(allCars);
-    } else {
-      res.status(404).json({ msg: `We could not find ${req.query.status} cars with  within the price range of ${req.query.min_price} and ${req.query.max_price}` });
-    }
-  } else if (req.query.state && req.query.status) {
-    const foundState = allCars.some(car => car.state === req.query.state);
-    if (foundState) {
-      allCars = allCars.filter(car => car.state === req.query.state);
-    } else {
-      res.status(404).json({ msg: `No car with the state of ${req.query.state}` });
-    }
-    const foundStatus = allCars.some(car => car.status === req.query.status);
-    if (foundStatus) {
-      allCars = allCars.filter(car => car.status === req.query.status);
-      res.status(200).json(allCars);
-    } else {
-      res.status(404).json({ msg: `No car was found with the status of ${req.query.status}` });
-    }
-  } else if (req.query.status && req.query.manufacturer) {
-    const foundStatus = allCars.some(car => car.status === req.query.status);
-    if (foundStatus) {
-      allCars = allCars.filter(car => car.status === req.query.status);
-    } else {
-      res.status(404).json({ msg: `No car with the state of ${req.query.state}` });
-    }
-    const foundManufacturer = allCars.some(car => car.manufacturer === req.query.manufacturer);
-    if (foundManufacturer) {
-      allCars = allCars.filter(car => car.manufacturer === req.query.manufacturer);
-      res.status(200).json(allCars);
-    } else {
-      res.status(404).json({ msg: `No car was found with the make of ${req.query.manufacturer}` });
-    }
-  } else if (req.query.status) {
-    const found = allCars.some(car => car.status === req.query.status);
-    if (found) {
-      allCars = allCars.filter(car => car.status === req.query.status);
-      res.status(200).json(allCars);
-    } else {
-      res.status(404).json({ msg: `No car was found with the status of ${req.query.status}` });
-    }
-  } else if (req.query.body_type) {
-    const foundBodyType = allCars.some(car => car.body_type === req.query.body_type);
-    if (foundBodyType) {
-      allCars = allCars.filter(car => car.body_type === req.query.body_type);
-      res.status(200).json(allCars);
-    } else {
-      res.status(404).json({ msg: `No car was found with the body_type of ${req.query.body_type}` });
-    }
-  } else {
-    res.status(200).json(cars);
-  }
+  });
 };
+
 // GET SPECIFIC CAR
 const getCar = (req, res) => {
   const ad = req.params;
@@ -97,8 +57,8 @@ const getCar = (req, res) => {
         message: 'error encountered',
       });
     } else if (dbres.rows.length === 0) {
-      res.status(403).json({
-        message: 'error encountered, Invalid car selection',
+      res.status(404).json({
+        message: 'No car found!!',
       });
     } else {
       const carad = dbres.rows;
@@ -215,4 +175,4 @@ module.exports = {
   postCar,
   patchCar,
   deleteCar,
-};
+}
