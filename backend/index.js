9f43b570ba635f1df99f19ee6fbb1450d3d53821
Adedayo-A <adesegunadedayo@hotmail.com>
2019-06-25 12:58:39 +0100

    chore(fixes):fix heroku crash error

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const morgan = require('morgan');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
