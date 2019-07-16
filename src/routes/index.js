818610a331ee29f38da3779e1501e270bc9cdafa
Adedayo-A <adesegunadedayo@hotmail.com>
2019-07-16 22:40:14 +0100

    (fix test):fix failing tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,10 +31,16 @@ const router = express.Router();
 // CAR ADs API ROUTES
 router.get('/api/v1/car/ads', tokenAuth, getadsByOwner);
 router.get('/api/v1/car/', tokenAuth, getCars);
+router.get('/car/', tokenAuth, getCars);
 router.get('/api/v1/car/:id', tokenAuth, getCar);
+router.get('/car/:id', tokenAuth, getCar);
 router.post('/api/v1/car/', tokenAuth, postCar);
+router.post('/car', tokenAuth, postCar);
 router.patch('/api/v1/car/:id/', tokenAuth, patchCar);
+router.patch('/car/:id/status', tokenAuth, patchCar);
+router.patch('/car/:id/price', tokenAuth, patchCar);
 router.delete('/api/v1/car/:id/', tokenAuth, deleteCar);
+router.delete('/car/:id/', tokenAuth, deleteCar);
 router.get('/api/v1/car/carorders', tokenAuth, getCarOrders);
 router.patch('/api/v1/car/:id/carorders', tokenAuth, updateCarOrders);
 
@@ -42,16 +48,20 @@ router.patch('/api/v1/car/:id/carorders', tokenAuth, updateCarOrders);
 // USERS API ROUTES
 router.patch('/api/v1/users/auth/update', tokenAuth, updateUser);
 router.post('/api/v1/users/auth/signup', validateInfo, signUp);
+router.post('/auth/signup', validateInfo, signUp);
 router.post('/api/v1/users/auth/signin', verifyUser);
 router.get('/api/v1/users/auth/getuser', tokenAuth, getAUser);
 router.post('/api/v1/users/auth/tokenverify', tokenVerify);
+router.post('/auth/signin', tokenVerify);
 
 
 // ORDERS API ROUTES
 router.get('/api/v1/order/user', tokenAuth, getMyOrders);
 router.get('/api/v1/order/:orderid', tokenAuth, getAnOrder);
 router.post('/api/v1/order/:id', tokenAuth, postOrder);
+router.post('/order', tokenAuth, postOrder);
 router.patch('/api/v1/order/:id/', tokenAuth, patchOrder);
+router.patch('/order/:id/price', tokenAuth, patchOrder);
 router.delete('/api/v1/order/:id/', tokenAuth, deleteOrder);
 
 
